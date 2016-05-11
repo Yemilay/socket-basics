daa56793501ae1fd62fd560ce05382592c047d68
Yemilay Romero <yemilay.romero@gmail.com>
2016-05-11 17:48:34 -0500

    Showing timestamp on UI

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var PORT = process.env.PORT || 3000;    // This line is x Heroku
+var moment = require('moment');
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
@@ -6,18 +7,21 @@ var io = require('socket.io')(http);
 
 app.use(express.static(__dirname + '/public'));
 
-// "on" listen for events
-io.on('connection', function (socket) {
+io.on('connection', function (socket) { // "on" listen for events
     console.log('User connected via socket.io!');
 
     socket.on('message', function (message) {
         console.log('Message recived: ' + message.text);
 
+        message.timestamp = moment().valueOf();
         io.emit('message', message);
     });
 
+    // tiemstamp property - JavaScript timestamp (milliseconds)
+
     socket.emit('message', {
-        text: 'Welcome to the chat application!'
+        text: 'Welcome to the chat application!',
+        timestamp: moment().valueOf()
     });
 });
 
